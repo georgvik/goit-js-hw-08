e5d0d3daa26537c26802aad32f5dcc7e2b317602
Yuriy Apanasenko <gepa9b@gmail.com>
2023-07-06 20:02:54 +0300

    homework redy

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+const imageEl = createImageGallery(galleryItems);
+
+
+
+function createImageGallery (items) {
+    return items.map(
+        ({preview, original, description}) => {
+            return `<li class="gallery__item"><a class = "gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"
+            data-source="${original}"
+            alt="${description}"/>
+            </a></li>
+            `;
+        }).join("")
+}
+galleryEl.insertAdjacentHTML('beforeend', imageEl);
+// galleryEl.addEventListener('click', onClick);
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    captionDelay: 250, 
+    captionsData: "alt",
+
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    email: document.querySelector('[name="email"]'),
+    textarea: document.querySelector('[name="message"]'),
+    form: document.querySelector('form'),
+  };
+
+  let feedbackData = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  const recoverData = () => {
+    refs.email.value = feedbackData.email || '';
+    refs.textarea.value = feedbackData.message || '';
+  };
+  recoverData();
+  const updateFeedbackData = event => {
+    feedbackData[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackData));
+  };
+  const formSubmit = event => {
+    event.preventDefault();
+    console.log(feedbackData);
+    localStorage.removeItem('feedback-form-state');
+    event.currentTarget.reset();
+    feedbackData = {};
+  };
+  refs.form.addEventListener('input', throttle(updateFeedbackData, 500));
+  refs.form.addEventListener('submit', formSubmit);
